176e031eedf6ec7f8cfe147ab5bcb45a9ff1f8fd
Chroma-God <86387797+Chroma-God@users.noreply.github.com>
2022-06-21 18:44:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+
 function setup() 
 {
   video = createCapture(VIDEO);
@@ -11,6 +19,11 @@ video.position(160, 150)
 }
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML= "Width and Height of a text will be" + difference + "px";
+    textSize(difference);
+    fill('#F90093');
+    stroke('#F90093');
+    text("Krish",noseX, noseY);
 }
 
 function modelLoaded(){
@@ -21,5 +34,14 @@ function gotPoses(results)
 {
     if(results.length > 0){
         console.log(results);
-    }
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY=" + noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    
+       console.log("leftWristX =" + leftWristX + "rightWristX =" + rightWristX + "difference = " + difference);
+      }
 }
